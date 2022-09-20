83b570b3b415864f09997b036b9495fe032ff7a6
Helmi Nadilla Yahya <110373373+helmiyahyaa@users.noreply.github.com>
2022-09-20 22:06:00 +0800

    Delete Berau directory

--- Berau/css/javascript.js
+++
@@ -1,9 +0,0 @@
-const humburger = document.querySelector(".hamburger");
-const navMenu= document.querySelector(".navigasi-menu");
-
-humburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    humburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-}
